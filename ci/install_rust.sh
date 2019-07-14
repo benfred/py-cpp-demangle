f5d282a82013ef4beb986b1e0717180e1778444f
Ben Frederickson <github@benfrederickson.com>
2019-07-14 16:36:59 -0700

    CI fixes

--- ci/install_rust.sh
+++ ci/install_rust.sh
@@ -1,5 +1,7 @@
 if [ ! -d ~/rust-installer ]; then 
     mkdir ~/rust-installer
     curl -sL https://static.rust-lang.org/rustup.sh -o ~/rust-installer/rustup.sh
-    sh ~/rust-installer/rustup.sh --spec=nightly --disable-sudo -y
+    sh ~/rust-installer/rustup.sh -y
+    source $HOME/.cargo/env
+    rustup default nightly
 fi
